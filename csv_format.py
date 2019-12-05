713a0efb86b173dde2056626e2129f247c5bb4b5
joechang0113 <joechang0113@gmail.com>
2019-12-05 17:47:57 +0800

    add format to dectionary

--- csv_format.py
+++ csv_format.py
@@ -1,7 +1,14 @@
 import csv
 
 with open('./jamesbond.csv') as csvfile:
-    data = csv.reader(csvfile)
-    passheader = next(data)
+    # 每一列的 list 直接輸出
+    # data = csv.reader(csvfile)
+    # passheader = next(data)
+    # for row in data:
+    #     print(row)
+
+    # 讀取後，轉為 Python 的 dictionary 格式
+    data = csv.DictReader(csvfile)
     for row in data:
-        print(row)
+        print(row['Film'], row['Year'], row['Actor'],
+              row['Director'], row['Box Office'], row['Budget'], row['Bond Actor Salary'],)
